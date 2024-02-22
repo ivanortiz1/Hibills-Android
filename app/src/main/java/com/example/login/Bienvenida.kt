eca6c530c8b9e3f66b7f5128828a730d6bd20be0
hugo <hugomartinezpriego@gmail.com>
2024-02-22 19:22:51 +0100

    version3

--- app/src/main/java/com/example/login/Bienvenida.kt
+++ app/src/main/java/com/example/login/Bienvenida.kt
@@ -5,18 +5,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 
 class Bienvenida : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
+        Toast.makeText(this, "Es necesario aceptar los términos y condiciones", Toast.LENGTH_SHORT).show()
 
         val nombreUsuario1 = intent.getStringArrayExtra("nombreUsuario")
 
         val txtSaludo = findViewById<TextView>(R.id.txtSaludo)
         txtSaludo.text = "!Hola, $nombreUsuario1!"
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            val id = item.itemId
+            if (id == R.id.action_opcion2) {
+                val intent1 = Intent(this, registro::class.java)
+                startActivity(intent1)
+                return true
+            }
+            if (id == R.id.action_opcion3) {
+                val intent2 = Intent(this, login::class.java)
+                startActivity(intent2)
+                return true
+            }
+            if (id == R.id.action_opcion4) {
+                val checkBoxTerminos = findViewById<CheckBox>(R.id.checkBoxTerminos)
+                if (checkBoxTerminos.isChecked) {
+                    val intent3 = Intent(this, pantalla_principal::class.java)
+                    startActivity(intent3)
+                    return true
+                } else {
+                    Toast.makeText(this, "Es necesario aceptar los términos y condiciones", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return super.onOptionsItemSelected(item)
+        }
+
     }
 }
 
