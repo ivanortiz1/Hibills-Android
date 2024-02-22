8d60e63a478bc4d0182f17d012049a6967a935eb
hugo <hugomartinezpriego@gmail.com>
2024-02-22 19:23:42 +0100

    version3

--- app/src/main/java/com/example/login/Bienvenida.kt
+++ app/src/main/java/com/example/login/Bienvenida.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 
@@ -20,31 +19,6 @@ class Bienvenida : AppCompatActivity() {
 
         val txtSaludo = findViewById<TextView>(R.id.txtSaludo)
         txtSaludo.text = "!Hola, $nombreUsuario1!"
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            val id = item.itemId
-            if (id == R.id.action_opcion2) {
-                val intent1 = Intent(this, registro::class.java)
-                startActivity(intent1)
-                return true
-            }
-            if (id == R.id.action_opcion3) {
-                val intent2 = Intent(this, login::class.java)
-                startActivity(intent2)
-                return true
-            }
-            if (id == R.id.action_opcion4) {
-                val checkBoxTerminos = findViewById<CheckBox>(R.id.checkBoxTerminos)
-                if (checkBoxTerminos.isChecked) {
-                    val intent3 = Intent(this, pantalla_principal::class.java)
-                    startActivity(intent3)
-                    return true
-                } else {
-                    Toast.makeText(this, "Es necesario aceptar los términos y condiciones", Toast.LENGTH_SHORT).show()
-                }
-            }
-            return super.onOptionsItemSelected(item)
-        }
 
     }
 }
