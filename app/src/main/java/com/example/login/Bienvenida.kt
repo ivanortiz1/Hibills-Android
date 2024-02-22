83343cacc6ac3eacd61bf713b9c2257e427f6550
hugo <hugomartinezpriego@gmail.com>
2024-02-22 19:05:19 +0100

    version3

--- app/src/main/java/com/example/login/Bienvenida.kt
+++ app/src/main/java/com/example/login/Bienvenida.kt
@@ -9,25 +9,6 @@ import android.widget.TextView
 
 class Bienvenida : AppCompatActivity() {
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_principal, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == R.id.action_opcion2) {
-            val intent1 = Intent(this, registro::class.java)
-            this.startActivity(intent1)
-            return true
-        }
-        if (id == R.id.action_opcion3) {
-            val intent2 = Intent(this, login::class.java)
-            this.startActivity(intent2)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bienvenida)
