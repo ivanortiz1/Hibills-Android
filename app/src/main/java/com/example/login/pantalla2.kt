0fb0a7cb6c64e49ed7dcb529458081dcc54d872f
hugo <hugomartinezpriego@gmail.com>
2024-02-27 18:22:59 +0100

    version7

--- app/src/main/java/com/example/login/pantalla2.kt
+++ app/src/main/java/com/example/login/pantalla2.kt
@@ -1,11 +1,20 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class pantalla2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla2)
+
+        val buttonPantallaCsesion = findViewById<Button>(R.id.buttonpantallacsesion)
+        buttonPantallaCsesion.setOnClickListener {
+            val intent = Intent(this, pantalla3::class.java)
+            startActivity(intent)
+        }
+
     }
 }
